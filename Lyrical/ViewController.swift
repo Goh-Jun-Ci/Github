cb9459c5e41ab35f4530da4f95e0aa09626d1c50
Goh-Jun-Ci <38309344+Goh-Jun-Ci@users.noreply.github.com>
2018-10-01 18:39:14 +0800

    Add files via upload

---
+++ Lyrical/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Lyrical
+//
+//  Created by Goh Jun Ci on 17/7/18.
+//  Copyright © 2018 Jun Ci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var pitchScore: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        }
+    }
+
+
+func didReceiveMemoryWarning() {
+    didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+
+
